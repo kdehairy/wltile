5a01325f424c5ee16b6814487cedb9d54d89a81e
Khaled Eldoheiri <khaled@eldoheiri.com>
2025-04-04 19:43:22 +0200

    fix: Positioning a rotated output
    for a rotated output, the width and hight must be swapped before doing
    any calculations.

--- src/functions/position.rs
+++ src/functions/position.rs
@@ -1,4 +1,5 @@
-use tracing::info;
+use tracing::{debug, info};
+use wayland_client::protocol::wl_output::Transform;
 
 use crate::{
     cli::{Alignment, Relation},
@@ -31,9 +32,25 @@ pub fn exec(target_setup: &TargetSetup, client: &wlr_client::Client) -> Result<(
     client.update_configurations(&request)
 }
 
+fn is_vertical(head: &Head<'_>) -> bool {
+    match head.transform() {
+        Transform::Normal |
+        Transform::_180 |
+        Transform::Flipped |
+        Transform::Flipped180 => false,
+        Transform::_90 |
+        Transform::_270 |
+        Transform::Flipped90 |
+        Transform::Flipped270 => true,
+        _ => panic!("Unexpected value for transform"),
+    }
+}
+
 #[allow(clippy::cast_possible_truncation, clippy::as_conversions)]
 fn build_reference_request<'a>(target_setup: &'a TargetSetup) -> HeadUpdateRequest<'a> {
-    let (target_size, reference_size) = scaled_sizes(target_setup);
+    let (target_size, reference_size) = scaled_corrected_sizes(target_setup);
+    debug!("{} size: {target_size}", target_setup.target.name());
+    debug!("{} size: {reference_size}", target_setup.reference.name());
     let mut position = match target_setup.relation {
         Relation::RightOf => Point(0, 0),
         Relation::LeftOf => target_size,
@@ -63,7 +80,7 @@ fn build_reference_request<'a>(target_setup: &'a TargetSetup) -> HeadUpdateReque
 }
 
 fn build_target_request<'a>(target_setup: &'a TargetSetup) -> HeadUpdateRequest<'a> {
-    let (target_size, reference_size) = scaled_sizes(target_setup);
+    let (target_size, reference_size) = scaled_corrected_sizes(target_setup);
     let mut position = match target_setup.relation {
         Relation::RightOf => reference_size,
         Relation::LeftOf => Point(0, 0),
@@ -71,7 +88,7 @@ fn build_target_request<'a>(target_setup: &'a TargetSetup) -> HeadUpdateRequest<
     let position = match target_setup.alignment {
         Alignment::AlignBottom => {
             position.1 = if target_size.1 < reference_size.1 {
-                i32::abs(target_size.1.saturating_sub(reference_size.1))
+                target_size.1.saturating_sub(reference_size.1).abs()
             } else {
                 0
             };
@@ -92,19 +109,36 @@ fn build_target_request<'a>(target_setup: &'a TargetSetup) -> HeadUpdateRequest<
 }
 
 #[allow(clippy::cast_possible_truncation, clippy::as_conversions)]
-fn scaled_sizes(target_setup: &TargetSetup) -> (Point, Point) {
+fn scaled_corrected_sizes(target_setup: &TargetSetup) -> (Point, Point) {
     let target_h = f64::from(target_setup.target.mode().size().1) / target_setup.target.scale();
     let target_h = target_h.round() as i32;
 
     let target_w = f64::from(target_setup.target.mode().size().0) / target_setup.target.scale();
     let target_w = target_w.round() as i32;
+    debug!("{} size: {target_w} x {target_h}", target_setup.target.name());
 
     let reference_h = f64::from(target_setup.reference.mode().size().1) / target_setup.reference.scale();
     let reference_h = reference_h.round() as i32;
 
     let reference_w = f64::from(target_setup.reference.mode().size().0) / target_setup.reference.scale();
     let reference_w = reference_w.round() as i32;
+    debug!("{} size: {reference_w} x {reference_h}", target_setup.reference.name());
 
-    (Point(target_w, target_h), Point(reference_w, reference_h))
+    let target = if is_vertical(&target_setup.target) {
+        Point(target_h, target_w)
+    } else {
+        debug!("{} is vertical", target_setup.target.name());
+        Point(target_w, target_h)
+    };
+    let reference = if is_vertical(&target_setup.reference) {
+        Point(reference_h, reference_w)
+    } else {
+        debug!("{} is vertical", target_setup.reference.name());
+        Point(reference_w, reference_h)
+    };
+    debug!("{} corrected size: {target}", target_setup.target.name());
+    debug!("{} corrected size: {reference}", target_setup.reference.name());
+
+    (target, reference)
 }
 
